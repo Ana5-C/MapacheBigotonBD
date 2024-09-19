7f3ee40863f592a3aea42fd597c953d780ac5fbd
Ana Cristina Martinez <am8794041@gmail.com>
2024-09-19 13:03:50 -0600

    commit carpeta
    se agrego la carpeta de intellij

---
+++ Barberia/src/test/java/com/MapacheBD/Barberia/BarberiaBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MapacheBD.Barberia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberiaBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
